04c999dc7f690ecf22dbb5365d60572235a1b1e9
zhuxy <zhuxyid@gmail.com>
2018-11-20 17:32:42 +0800

    add

--- web/models.py
+++ web/models.py
@@ -9,7 +9,7 @@ class Information(models.Model):
     gitlink = models.URLField(verbose_name='GIT链接',null=True)
     wblink = models.URLField(verbose_name='WEIBO链接',null=True)
     inslink = models.URLField(verbose_name='INS链接',null=True)
-    wxlink = models.URLField(verbose_name='WX链接',null=True)
     img = models.ImageField(verbose_name='头像',null=True,upload_to='static/img/')
+    wximg = models.ImageField(verbose_name='微信公众号头像',null=True,upload_to='static/img/wx/')
     def __str__(self):
         return '个人设置'
